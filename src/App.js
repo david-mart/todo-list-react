ecf24d7931aee73bd5d505370a41dcd6b334b99b
david-mart <david.mart@sadasystems.com>
2017-07-11 10:02:52 -0700

    final changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import Paper from "material-ui/Paper";
+import Items from "./Items";
+import NewItem from "./NewItem";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: [
+        { value: true, label: "Learn React" },
+        { value: false, label: "Watch Rick and Morty" }
+      ]
+    };
+    this.checkItem = this.checkItem.bind(this);
+    this.addNewItem = this.addNewItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+
+  checkItem(index) {
+    const items = this.state.items.map((item, i) => {
+      if (i === index) {
+        item.value = !item.value;
+      }
+      return item;
+    });
+    this.setState({ items });
+  }
+
+  deleteItem(index) {
+    const items = this.state.items.filter((item, i) => i !== index);
+    this.setState({ items });
+  }
+
+  addNewItem(item) {
+    const items = [...this.state.items, item];
+    this.setState({ items });
+  }
+
   render() {
+    const { items } = this.state;
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <Paper>
+          <NewItem onSubmit={this.addNewItem} />
+          <Items
+            items={items}
+            onItemValueChange={this.checkItem}
+            onDeleteClicked={this.deleteItem}
+          />
+        </Paper>
+      </MuiThemeProvider>
     );
   }
 }
